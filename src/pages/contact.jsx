fbd1e35b75ce94a1417f84d9bfa1d6fb1a9da2d5
Mohosin999 <hasanmohosin998@gmail.com>
2023-03-18 09:54:48 +0600

    successfully works all pages

---
+++ src/pages/contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <h2>I am contact</h2>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/pages/products.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <h2>I am products</h2>
+    </div>
+  );
+};
+
+export default Products;
