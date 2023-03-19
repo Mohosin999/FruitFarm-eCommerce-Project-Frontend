e53923b092a5cae679829d2d8cb29c1dec535b3a
Mohosin999 <hasanmohosin998@gmail.com>
2023-03-19 17:12:37 +0600

    finally worked fetch data

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import axios from "axios";
 
-const Products = (props) => {
+const Products = ({ products }) => {
   return (
     <div className="container mx-auto px-5">
-      <section class="text-gray-400 bg-gray-900 body-font">
-        <div class="container px-5 py-5 mx-auto">
-          <div class="flex flex-wrap -m-4">
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
+      <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="container px-5 py-5 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
                 <img
                   alt="ecommerce"
-                  class="object-cover object-center w-full h-full block"
+                  className="object-cover object-center w-full h-full block"
                   src="https://dummyimage.com/420x260"
                 />
               </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  {products.data[0].attributes.category}
                 </h3>
-                <h2 class="text-white title-font text-lg font-medium">
-                  {props.name}
+                <h2 className="text-white title-font text-lg font-medium">
+                  {products.data[1].attributes.title}
                 </h2>
-                <p class="mt-1">$16.00</p>
+                <p className="mt-1">{products.data[0].attributes.price}$</p>
               </div>
             </div>
           </div>
@@ -31,20 +32,27 @@ const Products = (props) => {
   );
 };
 
-export async function getServerSideProps(context) {
-  let headers = {
-    Authorization:
-      "Bearer a29b5ae704168a93f1b354fe98e01cc6822cf9cc308c1483b1036d9eafd4db7b792b72a40f55142040ec1b0c6da0febbab965ff7c72103922cbc1279e0214630a80eb73e7a90d69e7a33d95c78f3bdade1cfc31ac75b7c948d58bd11ab73b4c5e46997f822c8a7928832a24d251b8409b3dfa7679e07af323c29b64a7f578d43",
-  };
-  let a = await fetch("http://localhost:1337/api/products?populate=*", {
-    headers: headers,
-  });
-  let products = await a.json();
-  console.log(products);
-
-  return {
-    props: { products },
-  };
+export async function getServerSideProps() {
+  try {
+    const response = await axios.get(
+      "http://127.0.0.1:1337/api/products?populate=*",
+      {
+        headers: {
+          Authorization:
+            "bearer fb02ce15a12ca5c7d6465a3605ef70a044ac8d0efed62af56580796f7b0aaf2f879c144da8f88dca0891ca5cac02fb4df4fa800382fcaff8eb26a4296091332c87be9fb8b91e6088e2f26a7d7555e51f6a5357c752dba19be2cd970f13b40fb06b8836e9e57d99527fe04f49844f5d91bba29027f286375c4d389f71e1fa351b",
+        },
+      }
+    );
+    const products = response.data;
+    return {
+      props: { products },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: { products: [] },
+    };
+  }
 }
 
 export default Products;
